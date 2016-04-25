4315db74011e01fb4b2082d517d31ba3cb2a387a
Martin Koistinen <mkoistinen@gmail.com>
2016-04-24 23:24:11 -0400

    Remove imgmath package as it breaks RTD

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,6 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.imgmath',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
