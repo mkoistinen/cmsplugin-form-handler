99a9b77479fde7ab9944c23b93994bf98b945649
Martin Koistinen <mkoistinen@gmail.com>
2017-10-08 13:18:18 -0400

    Comment and clarify

--- cmsplugin_form_handler/forms.py
+++ cmsplugin_form_handler/forms.py
@@ -7,6 +7,11 @@ from django import forms
 
 class FormPluginFormMixin(object):
     def __init__(self, source_url, instance, **kwargs):
-        self.fields['cmsplugin_form_source_url'] = forms.CharField(widget=forms.HiddenInput, initial=source_url)
+        """
+        Extracts what we need from the modified signature, then instantiates
+        the form as usual.
+        """
         super(FormPluginFormMixin, self).__init__(**kwargs)
+        self.fields['cmsplugin_form_source_url'] = forms.CharField(
+            widget=forms.HiddenInput, initial=source_url)
         self.plugin_id = instance.pk
