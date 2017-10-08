84ba2bc7af8c867a0f4b4f106b1a77af1cbefed0
Martin Koistinen <mkoistinen@gmail.com>
2017-10-08 13:09:55 -0400

    Removes `*args` as a parameter, but adds `instance`

--- cmsplugin_form_handler/forms.py
+++ cmsplugin_form_handler/forms.py
@@ -6,6 +6,7 @@ from django import forms
 
 
 class FormPluginFormMixin(object):
-    def __init__(self, source_url, *args, **kwargs):
-        super(FormPluginFormMixin, self).__init__(*args, **kwargs)
+    def __init__(self, source_url, instance, **kwargs):
         self.fields['cmsplugin_form_source_url'] = forms.CharField(widget=forms.HiddenInput, initial=source_url)
+        super(FormPluginFormMixin, self).__init__(**kwargs)
+        self.plugin_id = instance.pk
